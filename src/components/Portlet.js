a07a23b8a4c562e486e7e63ed7683a5b94a8311b
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-28 17:41:02 -0200

    Add Portlet component

---
+++ src/components/Portlet.js
@@ -0,0 +1,23 @@
+import React, { PropTypes } from 'react';
+
+const Portlet = (props) => {
+  return (
+    <div className="portlet light">
+      <div className="portlet-title">
+        <div className="caption font-red-sunglo">
+          <span className="caption-subject bold uppercase">{props.title}</span>
+        </div>
+      </div>
+      <div className="portlet-body">
+        {props.children}
+      </div>
+    </div>
+  );
+};
+
+Portlet.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired
+};
+
+export default Portlet;
