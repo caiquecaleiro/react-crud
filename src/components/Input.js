84d706207e64fa1459cde21ea007df9799a955fc
Caique Caleiro <caiquecaleiro@gmail.com>
2016-12-29 18:58:55 -0200

    Add required prop to check whether the field requires validation or not

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,14 @@
 import React, { PropTypes } from 'react';
 
-function Input({ input, label, type }) {
+function Input({ input, label, type, required, meta: { touched, error, invalid } }) {
   return (
-    <div className="form-group">
+    <div className={`form-group ${required ? 'required' : ''} 
+      ${required && touched && invalid ? 'has-danger' : ''}`}>
       <label>{label}</label>
       <input className="form-control" {...input} type={type} />
+      <div className="form-control-feedback">
+        {required && touched ? error : ''}
+      </div>
     </div>
   );
 }
@@ -13,6 +17,8 @@ Input.propTypes = {
   input: PropTypes.object.isRequired,
   label: PropTypes.string.isRequired,
   type: PropTypes.string.isRequired,
+  meta: PropTypes.object.isRequired,
+  required: PropTypes.bool
 };
 
 export default Input;
